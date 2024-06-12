45c8401f7e5712bd31fc67b635bd78cb56531ce9
Simon Voordouw <simonvoordouw@gmail.com>
2024-06-13 01:01:11 +0200

    touch support

--- src/input.rs
+++ src/input.rs
@@ -1,18 +1,26 @@
 use bevy::{
     app::{App, Plugin, Update},
+    asset::Assets,
     ecs::{
+        component::Component,
         entity::Entity,
-        event::EventWriter,
+        event::{EventReader, EventWriter},
         query::With,
         schedule::{
-            common_conditions::{in_state, not},
-            IntoSystemConfigs, SystemSet,
+            common_conditions::{in_state, not, resource_added, resource_exists_and_equals},
+            Condition, IntoSystemConfigs, SystemSet,
         },
-        system::{Commands, Query, Res},
+        system::{Commands, Local, Query, Res, ResMut, Resource},
     },
-    input::{mouse::MouseButton, ButtonInput},
-    math::Quat,
-    render::camera::Camera,
+    input::{
+        mouse::MouseButton,
+        touch::{TouchInput, TouchPhase},
+        ButtonInput,
+    },
+    math::{primitives::Circle, Quat, Rect, Vec2, Vec3},
+    prelude::default,
+    render::{camera::Camera, color::Color, mesh::Mesh},
+    sprite::{ColorMaterial, MaterialMesh2dBundle},
     transform::components::{GlobalTransform, Transform},
     window::{PrimaryWindow, Window},
 };
@@ -30,10 +38,22 @@ impl Plugin for PlayerInputPlugin {
         app.add_systems(
             Update,
             (
-                player_ship_input.run_if(in_state(GameState::Playing)),
+                (
+                    player_ship_mouse_input.run_if(resource_exists_and_equals(InputMode::Mouse)),
+                    player_ship_touch_input.run_if(resource_exists_and_equals(InputMode::Touch)),
+                )
+                    .run_if(in_state(GameState::Playing)),
                 stop_player_throttling.run_if(not(in_state(GameState::Playing))),
             )
                 .in_set(PlayerInputSet),
+        )
+        .add_systems(
+            Update,
+            spawn_shoot_button.run_if(
+                in_state(GameState::Playing)
+                    .and_then(resource_added::<InputMode>)
+                    .and_then(resource_exists_and_equals(InputMode::Touch)),
+            ),
         );
     }
 }
@@ -41,7 +61,13 @@ impl Plugin for PlayerInputPlugin {
 #[derive(SystemSet, PartialEq, Eq, Clone, Copy, Debug, Hash)]
 pub struct PlayerInputSet;
 
-pub fn player_ship_input(
+#[derive(Resource, PartialEq, Eq, Clone, Copy, Debug, Hash)]
+pub enum InputMode {
+    Mouse,
+    Touch,
+}
+
+pub fn player_ship_mouse_input(
     mut commands: Commands,
     mouse_input: Res<ButtonInput<MouseButton>>,
     mut player_query: Query<(Entity, &GlobalTransform, &mut Transform), (With<Player>, With<Ship>)>,
@@ -83,6 +109,82 @@ pub fn player_ship_input(
     }
 }
 
+#[derive(Default)]
+struct TouchState(Option<Vec2>);
+
+fn player_ship_touch_input(
+    mut commands: Commands,
+    mut touches: EventReader<TouchInput>,
+    windows: Query<&Window, With<PrimaryWindow>>,
+    mut player_query: Query<(Entity, &GlobalTransform, &mut Transform), (With<Player>, With<Ship>)>,
+    camera_query: Query<(&Camera, &GlobalTransform)>,
+    mut fire_projectile_event_writer: EventWriter<FireEvent>,
+    mut touch_state: Local<TouchState>,
+) {
+    let (camera, camera_global_transform) = camera_query.single();
+    let primary_window = windows.single();
+
+    let button_ratio = 0.2;
+    let button_size =
+        (primary_window.height() * button_ratio).min(primary_window.width() * button_ratio);
+
+    // Define the region for firing projectiles
+    let fire_button_region = Rect {
+        min: Vec2::new(
+            primary_window.width() / 2. - button_size,
+            -primary_window.height() / 2.,
+        ), // Adjust size as needed
+        max: Vec2::new(
+            primary_window.width() / 2.,
+            -primary_window.height() / 2. + button_size,
+        ),
+    };
+
+    for touch in touches.read() {
+        match touch.phase {
+            TouchPhase::Started | TouchPhase::Moved => {
+                *touch_state = TouchState(Some(touch.position));
+            }
+            TouchPhase::Ended | TouchPhase::Canceled => {
+                *touch_state = TouchState(None);
+            }
+        }
+    }
+
+    if let Some(touch_pos) = touch_state.0 {
+        // This could be a short tap or the start of a hold
+        let touch_world_pos = camera
+            .viewport_to_world_2d(camera_global_transform, touch_pos)
+            .expect("Touch position not in world coordinates");
+
+        if fire_button_region.contains(touch_world_pos) {
+            // Touch is in the right bottom corner, fire a projectile
+            for (player_entity, _, _) in player_query.iter() {
+                fire_projectile_event_writer.send(FireEvent {
+                    turret_entity: player_entity,
+                });
+            }
+        } else {
+            // Point ship towards touch location
+            for (_, player_global_transform, mut player_transform) in player_query.iter_mut() {
+                let direction = touch_world_pos - player_global_transform.translation().truncate();
+                let angle = direction.y.atan2(direction.x);
+                let target_rotation = Quat::from_rotation_z(angle - std::f32::consts::FRAC_PI_2);
+                player_transform.rotation = target_rotation;
+            }
+
+            for (player_entity, _, _) in player_query.iter() {
+                commands.entity(player_entity).insert(Throttling);
+            }
+        }
+    } else {
+        // Touch ended or cancelled, stop throttling
+        for (player_entity, _, _) in player_query.iter() {
+            commands.entity(player_entity).remove::<Throttling>();
+        }
+    }
+}
+
 pub fn stop_player_throttling(
     mut commands: Commands,
     player_query: Query<Entity, (With<Player>, With<Throttling>)>,
@@ -90,4 +192,35 @@ pub fn stop_player_throttling(
     for player_entity in player_query.iter() {
         commands.entity(player_entity).remove::<Throttling>();
     }
+}
+
+#[derive(Component)]
+struct ShootButton;
+
+fn spawn_shoot_button(
+    mut commands: Commands,
+    windows: Query<&Window, With<PrimaryWindow>>,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    let window = windows.single();
+    let button_ratio = 0.2;
+    let button_size = (window.height() * button_ratio).min(window.width() * button_ratio);
+    let mesh_handle = meshes.add(Circle {
+        radius: button_size / 2.,
+    });
+
+    commands.spawn((
+        ShootButton,
+        MaterialMesh2dBundle {
+            mesh: mesh_handle.into(),
+            material: materials.add(ColorMaterial::from(Color::RED)),
+            transform: Transform::from_translation(Vec3::new(
+                window.width() / 2. - button_size / 2.,
+                -window.height() / 2. + button_size / 2.,
+                10.,
+            )),
+            ..default()
+        },
+    ));
 }
--- src/ui.rs
+++ src/ui.rs
@@ -1,19 +1,31 @@
 use crate::{
     game_state::{GameResult, GameState},
+    input::InputMode,
     utils::cleanup,
 };
 use bevy::{
     app::{App, Plugin, Update},
     ecs::{
-        schedule::{common_conditions::in_state, IntoSystemConfigs, NextState, OnEnter, OnExit},
-        system::ResMut,
+        entity::Entity,
+        event::EventReader,
+        query::With,
+        schedule::{
+            common_conditions::in_state, Condition, IntoSystemConfigs, NextState, OnEnter, OnExit,
+            States,
+        },
+        system::{Query, ResMut},
     },
-    input::{keyboard::KeyCode, ButtonInput},
+    input::{
+        mouse::MouseButton,
+        touch::{TouchInput, TouchPhase, Touches},
+        ButtonInput,
+    },
     log::info,
     prelude::{
         default, AlignItems, AssetServer, BuildChildren, Color, Commands, Component, FlexDirection,
         JustifyContent, Name, NodeBundle, Res, Style, TextBundle, TextStyle, Val,
     },
+    time::{Time, Timer, TimerMode},
     ui::UiRect,
 };
 
@@ -23,12 +35,42 @@ impl Plugin for StartScreenPlugin {
     fn build(&self, app: &mut App) {
         app.add_systems(OnEnter(GameState::Menu), spawn_start_screen)
             .add_systems(OnExit(GameState::Menu), cleanup::<StartScreen>)
-            .add_systems(Update, start_game.run_if(in_state(GameState::Menu)));
+            .init_state::<StartScreenState>()
+            .add_systems(OnExit(StartScreenState::Start), cleanup::<ClickOrTap>)
+            .add_systems(
+                Update,
+                set_input_mode
+                    .run_if(in_state(GameState::Menu).and_then(in_state(StartScreenState::Start))),
+            )
+            .add_systems(OnEnter(StartScreenState::Instructions), spawn_instructions)
+            .add_systems(
+                Update,
+                start_game.run_if(
+                    in_state(GameState::Menu).and_then(in_state(StartScreenState::Instructions)),
+                ),
+            )
+            .add_systems(
+                OnExit(StartScreenState::Instructions),
+                cleanup::<Instructions>,
+            );
     }
 }
 
 #[derive(Component)]
 pub struct StartScreen;
+
+#[derive(Component)]
+struct ClickOrTap;
+
+#[derive(Component)]
+struct Instructions;
+
+#[derive(States, Default, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum StartScreenState {
+    #[default]
+    Start,
+    Instructions,
+}
 
 const FONT_PATH: &str = "fonts/TurretRoad-ExtraLight.ttf";
 
@@ -62,58 +104,117 @@ fn spawn_start_screen(mut commands: Commands, asset_server: Res<AssetServer>) {
                 ),
             ));
 
-            parent
-                .spawn((
-                    Name::new("Instructions"),
-                    NodeBundle {
-                        style: Style {
-                            margin: UiRect::all(Val::Px(20.)),
-                            align_items: AlignItems::Center,
-                            flex_direction: FlexDirection::Column,
-                            ..default()
-                        },
-                        ..default()
-                    },
-                ))
-                .with_children(|parent| {
-                    let instruction_style = TextStyle {
-                        font: asset_server.load(FONT_PATH),
-                        font_size: 40.,
-                        color: Color::WHITE,
-                    };
-                    parent.spawn(TextBundle::from_section(
-                        "Point cursor to aim ship",
-                        instruction_style.clone(),
-                    ));
-                    parent.spawn(TextBundle::from_section(
-                        "Left click to fire thrusters",
-                        instruction_style.clone(),
-                    ));
-                    parent.spawn(TextBundle::from_section(
-                        "Right click to fire turret",
-                        instruction_style.clone(),
-                    ));
-                });
-
-            parent.spawn((
-                Name::new("Continue instructions text"),
-                TextBundle::from_section(
-                    "Press Space to start",
-                    TextStyle {
-                        font: asset_server.load(FONT_PATH),
-                        font_size: 40.,
-                        color: Color::WHITE,
-                    },
-                ),
-            ));
+            spawn_click_or_tap(parent, &asset_server);
         });
 }
 
+fn spawn_click_or_tap(parent: &mut bevy::prelude::ChildBuilder, asset_server: &AssetServer) {
+    parent.spawn((
+        Name::new("Click or tap text"),
+        ClickOrTap,
+        TextBundle::from_section(
+            "Click or tap to continue",
+            TextStyle {
+                font: asset_server.load(FONT_PATH),
+                font_size: 40.,
+                color: Color::WHITE,
+            },
+        ),
+    ));
+}
+
+fn set_input_mode(
+    mut commands: Commands,
+    mouse_input: Res<ButtonInput<MouseButton>>,
+    touches: Res<Touches>,
+    mut next_start_screen_state: ResMut<NextState<StartScreenState>>,
+) {
+    if mouse_input.just_pressed(MouseButton::Left) {
+        commands.insert_resource(InputMode::Mouse);
+        next_start_screen_state.set(StartScreenState::Instructions);
+    }
+
+    if touches.iter().next().is_some() {
+        commands.insert_resource(InputMode::Touch);
+        next_start_screen_state.set(StartScreenState::Instructions);
+    }
+}
+
+fn spawn_instructions(
+    mut commands: Commands,
+    start_screen_query: Query<Entity, With<StartScreen>>,
+    asset_server: Res<AssetServer>,
+    input_mode: Res<InputMode>,
+) {
+    let start_screen = start_screen_query.single();
+    commands.entity(start_screen).with_children(|parent| {
+        parent
+            .spawn((
+                Name::new("Instructions"),
+                NodeBundle {
+                    style: Style {
+                        margin: UiRect::all(Val::Px(20.)),
+                        align_items: AlignItems::Center,
+                        flex_direction: FlexDirection::Column,
+                        ..default()
+                    },
+                    ..default()
+                },
+            ))
+            .with_children(|parent| {
+                let instruction_style = TextStyle {
+                    font: asset_server.load(FONT_PATH),
+                    font_size: 40.,
+                    color: Color::WHITE,
+                };
+                parent.spawn(TextBundle::from_section(
+                    match *input_mode {
+                        InputMode::Mouse => "Point cursor to aim ship",
+                        InputMode::Touch => "Touch to aim ship",
+                    },
+                    instruction_style.clone(),
+                ));
+                parent.spawn(TextBundle::from_section(
+                    match *input_mode {
+                        InputMode::Mouse => "Hold click to fire thrusters",
+                        InputMode::Touch => "Hold touch to fire thrusters",
+                    },
+                    instruction_style.clone(),
+                ));
+                parent.spawn(TextBundle::from_section(
+                    match *input_mode {
+                        InputMode::Mouse => "Right click to fire turret",
+                        InputMode::Touch => "Tap right bottom corner to fire turret",
+                    },
+                    instruction_style.clone(),
+                ));
+            });
+
+        parent.spawn((
+            Name::new("Continue instructions text"),
+            TextBundle::from_section(
+                match *input_mode {
+                    InputMode::Mouse => "Click anywhere to start",
+                    InputMode::Touch => "Tap anywhere to start",
+                },
+                TextStyle {
+                    font: asset_server.load(FONT_PATH),
+                    font_size: 40.,
+                    color: Color::WHITE,
+                },
+            ),
+        ));
+    });
+}
+
 fn start_game(
-    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mouse_input: Res<ButtonInput<MouseButton>>,
+    mut touch_events: EventReader<TouchInput>,
     mut next_gamestate: ResMut<NextState<GameState>>,
 ) {
-    if keyboard_input.pressed(KeyCode::Space) {
+    if mouse_input.just_pressed(MouseButton::Left)
+        || touch_events.read().any(|t| t.phase == TouchPhase::Started)
+    {
         next_gamestate.set(GameState::Playing);
         info!("Starting game");
     }
@@ -123,23 +224,49 @@ pub struct FinishedScreenPlugin;
 
 impl Plugin for FinishedScreenPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(OnEnter(GameState::Finished), spawn_game_finished_screen)
+        app.init_state::<FinishedScreenState>()
+            .add_systems(OnEnter(GameState::Finished), spawn_game_finished_screen)
             .add_systems(OnExit(GameState::Finished), cleanup::<FinishedText>)
-            .add_systems(Update, restart_game.run_if(in_state(GameState::Finished)));
+            .add_systems(
+                Update,
+                (
+                    finished_screen_timer,
+                    restart_game.run_if(in_state(FinishedScreenState::PromptRestart)),
+                )
+                    .run_if(in_state(GameState::Finished)),
+            );
     }
 }
 
 #[derive(Component)]
-pub struct FinishedText;
+pub struct FinishedText {
+    timer: Timer,
+}
+
+#[derive(States, Default, Debug, Clone, Copy, PartialEq, Eq, Hash)]
+enum FinishedScreenState {
+    #[default]
+    Locked,
+    PromptRestart,
+}
+
+impl Default for FinishedText {
+    fn default() -> Self {
+        Self {
+            timer: Timer::from_seconds(3.0, TimerMode::Once),
+        }
+    }
+}
 
 fn spawn_game_finished_screen(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     game_result: Res<GameResult>,
+    mut next_finished_screen_state: ResMut<NextState<FinishedScreenState>>,
 ) {
     commands
         .spawn((
-            FinishedText,
+            FinishedText::default(),
             Name::new("Finished screen"),
             NodeBundle {
                 style: Style {
@@ -168,10 +295,35 @@ fn spawn_game_finished_screen(
                     },
                 ),
             ));
+        });
+
+    next_finished_screen_state.set(FinishedScreenState::Locked);
+}
+
+fn finished_screen_timer(
+    mut commands: Commands,
+    time: Res<Time>,
+    mut finished_text_query: Query<(Entity, &mut FinishedText)>,
+    input_mode: Res<InputMode>,
+    asset_server: Res<AssetServer>,
+    mut next_finished_screen_state: ResMut<NextState<FinishedScreenState>>,
+) {
+    let (finished_text_entity, mut finished_text) = finished_text_query.single_mut();
+
+    if !finished_text.timer.tick(time.delta()).just_finished() {
+        return;
+    }
+
+    commands
+        .entity(finished_text_entity)
+        .with_children(|parent| {
             parent.spawn((
                 Name::new("Restart text"),
                 TextBundle::from_section(
-                    "Press R to restart",
+                    match *input_mode {
+                        InputMode::Mouse => "Click to restart",
+                        InputMode::Touch => "Tap to restart",
+                    },
                     TextStyle {
                         font: asset_server.load(FONT_PATH),
                         font_size: 40.,
@@ -180,14 +332,19 @@ fn spawn_game_finished_screen(
                 ),
             ));
         });
+
+    next_finished_screen_state.set(FinishedScreenState::PromptRestart);
 }
 
 fn restart_game(
     mut commands: Commands,
-    keyboard_input: Res<ButtonInput<KeyCode>>,
+    mouse_input: Res<ButtonInput<MouseButton>>,
+    mut touch_events: EventReader<TouchInput>,
     mut next_gamestate: ResMut<NextState<GameState>>,
 ) {
-    if keyboard_input.pressed(KeyCode::KeyR) {
+    if mouse_input.just_pressed(MouseButton::Left)
+        || touch_events.read().any(|t| t.phase == TouchPhase::Started)
+    {
         commands.remove_resource::<GameResult>();
         next_gamestate.set(GameState::Playing);
         info!("Restarting game");
